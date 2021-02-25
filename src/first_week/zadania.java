9be4f31cf5fc01ee9435755235ad9cb139c05256
Kacper <kacperfil1321@wp.pl>
2021-02-25 13:04:03 +0100

    Created zadania

---
+++ src/first_week/zadania.java
@@ -0,0 +1,36 @@
+package first_week;
+
+public class zadania {
+    public static void main(String[] args) {
+
+        // Zadanie 1
+        int X = 14;
+        double Y = 3.56;
+        char Z = '@';
+        String A = "Rower";
+
+        System.out.println(X);
+        System.out.println(Y);
+        System.out.println(Z);
+        System.out.println(A);
+
+        // Zadanie 2
+        int liczbaA = 4;
+        int liczbaB = 17;
+
+        int suma = (liczbaA * liczbaA) + (liczbaB * liczbaB);
+        System.out.println(suma);
+
+        // Zadanie 3
+        int bokA = 7;
+        double Pole = (bokA * bokA) * Math.sqrt(3) / 4;
+        System.out.println(Pole);
+
+        //Zadanie 4
+        String Imie = "Kacper";
+        String Nazwisko = "Filipski";
+
+        System.out.println(Imie + ' ' + Nazwisko);
+
+    }
+}
